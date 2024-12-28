9f2279da6fb04d669ac5340bf3ad7abae0bc5f6d
NethminiSandunika <nethminid92@gmail.com>
2024-12-28 10:12:12 +0530

    moved the images file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Recipe from './components/Recipe';
+import Popular from './components/Popular';
+import Delivery from './components/Delivery';
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <Home />
+        <Recipe />
+        <Popular />
+        <Delivery />
+        <Contact />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
